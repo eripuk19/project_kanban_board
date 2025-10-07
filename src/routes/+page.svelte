e8f93bee93a6f83a79644cd4ca623bb005952bfa
EriPuk19 <eripuk19@htl-shhkoder.com>
2025-10-07 11:04:10 +0200

    changed the style to pink

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,11 +49,11 @@
   
   <h1 class="text-center">Project Kanban Board</h1>
   
-  <main class="p-32 w-full bg-gray-400 h-[700px] flex justify-between items-center">
+  <main class="p-32 w-full bg-pink-400 h-[700px] flex justify-between items-center">
     <section class="p-3 bg-white h-[400px] w-[300px] space-y-2" ondragover={dragOver} ondrop={dropToDo}>
       <h2>Do</h2>
       {#each Do as item (item)}
-        <article ondragstart={(event) => handleDragStart(item, event)} class="p-4 bg-blue-200 cursor-grab" draggable="true" animate:flip>
+        <article ondragstart={(event) => handleDragStart(item, event)} class="p-4 bg-pink-200 cursor-grab" draggable="true" animate:flip>
           {item}
         </article>
       {/each}
@@ -62,7 +62,7 @@
     <section class="p-3 bg-white h-[400px] w-[300px] space-y-2" ondragover={dragOver} ondrop={dropToDoing}>
       <h2>Doing</h2>
       {#each Doing as item (item)}
-        <article ondragstart={(event) => handleDragStart(item, event)} class="p-4 bg-blue-200 cursor-grab" draggable="true" animate:flip>
+        <article ondragstart={(event) => handleDragStart(item, event)} class="p-4 bg-pink-200 cursor-grab" draggable="true" animate:flip>
           {item}
         </article>
       {/each}
@@ -71,7 +71,7 @@
     <section class="p-3 bg-white h-[400px] w-[300px] space-y-2" ondragover={dragOver} ondrop={dropToDone}>
       <h2>Done</h2>
       {#each Done as item (item)}
-        <article ondragstart={(event) => handleDragStart(item, event)} class="p-4 bg-blue-200 cursor-grab" draggable="true" animate:flip >
+        <article ondragstart={(event) => handleDragStart(item, event)} class="p-4 bg-pink-200 cursor-grab" draggable="true" animate:flip >
           {item}
         </article>
       {/each}
@@ -80,7 +80,7 @@
     <section class="p-3 bg-white h-[400px] w-[300px] space-y-2" ondragover={dragOver} ondrop={dropToArchive}>
       <h2>Archive</h2>
       {#each Archive as item (item)}
-        <article ondragstart={(event) => handleDragStart(item, event)} class="p-4 bg-blue-200 cursor-grab" draggable="true" animate:flip >
+        <article ondragstart={(event) => handleDragStart(item, event)} class="p-4 bg-pink-200 cursor-grab" draggable="true" animate:flip >
           {item}
         </article>
       {/each}
